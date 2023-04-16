63a77277058c890ce42c051ee7dd10d6c92144c3
rubenforti <rubenforti27@gmail.com>
2023-04-16 04:26:39 +0200

    updated syntax in simultaneous fit code - changes need to be completed

--- sim_efficiency.py
+++ sim_efficiency.py
@@ -2,18 +2,16 @@
 """
 
 import ROOT
+import time
 import os
-import pickle
-from utilities import import_Steve_histos
-from results_utilities import res_manager_sim
+import sys
+from results_utilities import res_manager_sim, fit_quality
+from plot_functions import makeAndSavePlot
+from stat_functions import pearson_chi2_eval, llr_test_bkg
+from utilities import import_pdf_library
 
 
-def fit_on_bin(type_eff, workspace, cond, bin, test_bkg=False, verb=-1):
-    """
-    """
-
-
-def sim_efficiency(type_eff, bin_pt, bin_eta, results,
+def sim_efficiency(type_eff, workspace, bin, bkg_pdf,
                    same_smearing=True,
                    test_bkg=False,
                    enable_mcfit=False,
@@ -21,90 +19,113 @@ def sim_efficiency(type_eff, bin_pt, bin_eta, results,
     """
     """
 
-    h_data, h_mc, n_events, x = import_Steve_histos(
-        type_eff, [bin_pt], [bin_eta])
+    if type(workspace[f'PDF_pass_({bin[0]},{bin[1]})']) is ROOT.RooAddPdf:
+        # DA IMPLEMENTARE CORRETTAMENTE
+        sys.exit()
 
-    path = os.path.dirname(__file__)
-    ROOT.gSystem.cd(path)
+    elif type(workspace[f'PDF_pass_({bin[0]},{bin[1]})']) is ROOT.TObject and type(workspace[f'PDF_fail_({bin[0]},{bin[1]})']) is ROOT.TObject:
 
-    # Backgrounds
-    # -----------
-    tau_p = ROOT.RooRealVar("tau_p", "tau_p", -10, 0)
-    expo_p = ROOT.RooExponential("expo_p", "expo_p", x, tau_p)
-    tau_f = ROOT.RooRealVar("tau_f", "tau_f", -10, 0)
-    expo_f = ROOT.RooExponential("expo_f", "expo_f", x, tau_f)
+        histo_data_pass = workspace[f"Minv_data_pass_({bin[0]},{bin[1]})"]
+        # histo_mc_pass = workspace[f"Minv_mc_pass_({bin[0]},{bin[1]})"]
 
-    # PDFs from MC datasets
-    # ---------------------
-    pdf_mc_pass = ROOT.RooHistPdf("pdf_mc_pass", "pdf_mc_pass", x, h_mc[1])
-    pdf_mc_fail = ROOT.RooHistPdf("pdf_mc_fail", "pdf_mc_fail", x, h_mc[0])
+        histo_data_fail = workspace[f"Minv_data_fail_({bin[0]},{bin[1]})"]
+        # histo_mc_fail = workspace[f"Minv_mc_fail_({bin[0]},{bin[1]})"]
 
-    # Smearing functions
-    # ------------------
-    mean = ROOT.RooRealVar("mean", "mean", 0, -2, 2)
-    sigma = ROOT.RooRealVar("sigma", "sigma", 0.5, 0.001, 2)
-    smear = ROOT.RooGaussian("smear", "smear", x, mean, sigma)
-    if same_smearing is not True:
-        mean_f = ROOT.RooRealVar("mean_f", "mean_f", 0, -2, 2)
-        sigma_f = ROOT.RooRealVar("sigma_f", "sigma_f", 0.5, 0.001, 2)
-        smear_fail = ROOT.RooGaussian(
-            "smear_fail", "smear_fail", x, mean_f, sigma_f)
-    else:
-        smear_fail = smear
+        axis = workspace[f"x_sim_({bin[0]},{bin[1]})"]
 
-    # Convolutions with FFT
-    # ---------------------
-    x.setBins(1000, "cache")
-    conv_pass = ROOT.RooFFTConvPdf("conv", "conv", x, pdf_mc_pass, smear, 3)
-    conv_pass.setBufferFraction(0.1)
-    conv_fail = ROOT.RooFFTConvPdf(
-        "conv", "conv", x, pdf_mc_fail, smear_fail, 3)
-    conv_fail.setBufferFraction(0.1)
+        # Backgrounds
+        # -----------
+        tau_p = ROOT.RooRealVar("tau_p", "tau_p", -10, 0)
+        expo_p = ROOT.RooExponential("expo_p", "expo_p", axis, tau_p)
+        tau_f = ROOT.RooRealVar("tau_f", "tau_f", -10, 0)
+        expo_f = ROOT.RooExponential("expo_f", "expo_f", axis, tau_f)
 
-    exp_ntot = n_events[0][0]+n_events[1][1]
-    Nsig_tot = ROOT.RooRealVar("Nsig_tot", "#signal events total", exp_ntot,
-                               0, exp_ntot+3*ROOT.TMath.Sqrt(exp_ntot))
-    efficiency = ROOT.RooRealVar("efficiency", "efficiency", 0, 1)
+        # PDFs from MC datasets
+        # ---------------------
+        pdf_mc_pass = ROOT.RooHistPdf(
+            "pdf_mc_pass", "pdf_mc_pass", axis,
+            workspace[f"Minv_mc_pass_({bin[0]},{bin[1]})"])
+        pdf_mc_fail = ROOT.RooHistPdf(
+            "pdf_mc_fail", "pdf_mc_fail", axis,
+            workspace[f"Minv_mc_fail_({bin[0]},{bin[1]})"])
 
-    Nsig_pass = ROOT.RooProduct(
-        "Nsig_pass", "Nsig_pass", [efficiency, Nsig_tot])
-    Nbkg_pass = ROOT.RooRealVar(
-            "nbkg_p", "#background events pass", 0, n_events[0][1])
+        # Smearing functions and FFT convolutions
+        # ---------------------------------------
+        mean = ROOT.RooRealVar("mean", "mean", 0, -2, 2)
+        sigma = ROOT.RooRealVar("sigma", "sigma", 0.5, 0.001, 2)
+        smear = ROOT.RooGaussian("smear", "smear", axis, mean, sigma)
 
-    sum_pass = ROOT.RooAddPdf("sum_pass", "sum_pass",
-                              [conv_pass, expo_p], [Nsig_pass, Nbkg_pass])
-    model_pass = ROOT.RooAddPdf(sum_pass)
+        axis.setBins(1000, "cache")
 
-    one_minus_eff = ROOT.RooPolyVar(
-        "one_minus_eff", "one_minus_eff", efficiency, [1, -1.])
-    Nsig_fail = ROOT.RooProduct("prod", "prod", [one_minus_eff, Nsig_tot])
+        conv_pass = ROOT.RooFFTConvPdf(
+            "conv", "conv", axis, pdf_mc_pass, smear, 3)
 
-    Nbkg_fail = ROOT.RooRealVar(
-        "nbkg_f", "#background events fail", 0, n_events[0][0])
+        if same_smearing is not True:
+            mean_f = ROOT.RooRealVar("mean_f", "mean_f", 0, -2, 2)
+            sigma_f = ROOT.RooRealVar("sigma_f", "sigma_f", 0.5, 0.001, 2)
 
-    sum_fail = ROOT.RooAddPdf("sum_fail", "sum_fail",
-                              [conv_fail, expo_f], [Nsig_fail, Nbkg_fail])
-    model_fail = ROOT.RooAddPdf(sum_fail)
+            smear_fail = ROOT.RooGaussian(
+                "smear_fail", "smear_fail", axis, mean_f, sigma_f)
 
-    sample = ROOT.RooCategory("sample", "sample")
-    sample.defineType("pass")
-    sample.defineType("fail")
+            conv_fail = ROOT.RooFFTConvPdf(
+                "conv", "conv", axis, pdf_mc_fail, smear_fail, 3)
+            conv_fail.setBufferFraction(0.1)
+        else:
+            conv_fail = ROOT.RooFFTConvPdf(
+                "conv", "conv", axis, pdf_mc_fail, smear, 3)
 
-    comb_dataset = ROOT.RooDataHist(
-        "combData", "combined datasets", ROOT.RooArgSet(x), Index=sample,
-        Import={"pass": h_data[1], "fail": h_data[0]})
+        conv_pass.setBufferFraction(0.1)
+        conv_fail.setBufferFraction(0.1)
 
-    simPdf = ROOT.RooSimultaneous("simPdf", "simultaneous pdf", sample)
-    simPdf.addPdf(model_pass, "pass")
-    simPdf.addPdf(model_fail, "fail")
+        expected_ntot = histo_data_pass.sumEntries()+histo_data_fail.sumEntries()
+        Nsig_tot = ROOT.RooRealVar(
+            "Nsig_tot", "#signal events total",
+            expected_ntot, 0, expected_ntot+3*ROOT.TMath.Sqrt(expected_ntot))
 
-    fitResult = simPdf.fitTo(comb_dataset, Save=True,
-                             PrintLevel=-1, Extended=True)
+        efficiency = ROOT.RooRealVar("efficiency", "efficiency", 0, 1)
 
-    results.add_result(fitResult, bin_pt, bin_eta)
+        Nsig_pass = ROOT.RooProduct(
+            "Nsig_pass", "Nsig_pass", [efficiency, Nsig_tot])
+        Nbkg_pass = ROOT.RooRealVar(
+                "nbkg_p", "#background events pass", 0, histo_data_pass.sumEntries())
+
+        sum_pass = ROOT.RooAddPdf("sum_pass", "sum_pass",
+                                  [conv_pass, expo_p], [Nsig_pass, Nbkg_pass])
+        model_pass = ROOT.RooAddPdf(sum_pass)
+
+        one_minus_eff = ROOT.RooPolyVar(
+            "one_minus_eff", "one_minus_eff", efficiency, [1, -1.])
+        Nsig_fail = ROOT.RooProduct("prod", "prod", [one_minus_eff, Nsig_tot])
+
+        Nbkg_fail = ROOT.RooRealVar(
+            "nbkg_f", "#background events fail", 0, histo_data_fail.sumEntries())
+
+        sum_fail = ROOT.RooAddPdf("sum_fail", "sum_fail",
+                                  [conv_fail, expo_f], [Nsig_fail, Nbkg_fail])
+        model_fail = ROOT.RooAddPdf(sum_fail)
+
+        sample = ROOT.RooCategory("sample", "sample")
+        sample.defineType("pass")
+        sample.defineType("fail")
+
+        comb_dataset = ROOT.RooDataHist(
+            "combData", "combined datasets", ROOT.RooArgSet(axis), Index=sample,
+            Import={"pass": histo_data_pass, "fail": histo_data_fail})
+
+        simPdf = ROOT.RooSimultaneous("simPdf", "simultaneous pdf", sample)
+        simPdf.addPdf(model_pass, "pass")
+        simPdf.addPdf(model_fail, "fail")
+
+        fitResult = simPdf.fitTo(comb_dataset, Save=True,
+                                 PrintLevel=-1, Extended=True)
+
+        print(fitResult.floatParsFinal())
 
 
 if __name__ == '__main__':
+
+    path = os.path.dirname(__file__)
+    ROOT.gSystem.cd(path)
 
     custom_pdfs = ['RooCBExGaussShape',
                    'RooDoubleCBFast', 'RooCMSShape', 'my_double_CB']
@@ -120,10 +141,9 @@ if __name__ == '__main__':
 
     results = res_manager_sim()
 
-    for bin_pt in range(15):
-        for bin_eta in range(48):
-            sim_efficiency(t, bin_pt+1, bin_eta+1,
-                           results, same_smearing=False)
+    for bin_pt in range(1):
+        for bin_eta in range(1):
+            sim_efficiency(t, bin_pt+1, bin_eta+1, same_smearing=False)
 
     results.write("simult_eff_results.pkl")
 
